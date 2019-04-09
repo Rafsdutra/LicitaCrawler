30617c19d7663bb200388c8a111e6df850ff6922
Rafael <rafsdutra32@gmail.com>
2019-04-09 12:28:27 -0300

    Adicionada a classe sendMail

--- NCrawler/pipelines.py
+++ NCrawler/pipelines.py
@@ -6,16 +6,53 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from datetime import datetime
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from scrapy.exceptions import DropItem
+import smtplib
+
+
+
 
 
 class FilterDatePipeline(object):
     def process_item(self, item, spider):
         now = datetime.now()
-        date_licitacao  = int(item['numerocp'][0].split('/')[-1])
+        date_licitacao = int(item['numerocp'][0].split('/')[-1])
         if int(date_licitacao) == now.year:
             print(date_licitacao)
             return item
         else:
             raise DropItem("Licitação fora do ano atual")
+
+
+class sendMail(object):
+
+    def close_spider(self, spider):
+
+        print('Enviando Email com as informações de Licitação...')
+        from_email = "b89b239862ecb5"
+        to_email = "to@smtp.mailtrap.io"
+
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = to_email
+        msg['Subject'] = 'Licitacoes '
+
+        intro = "Licitacoes: \n\n"
+
+        body = 'Oi'
+        body = intro + body
+        msg.attach(MIMEText(body, 'plain'))
+
+        server = smtplib.SMTP("smtp.mailtrap.io", 2525)
+        server.starttls()
+        server.login(from_email, "f20851757ed914")
+        text = msg.as_string()
+        server.sendmail(from_email, to_email, text)
+        server.quit()
+
+        print('Email enviado! ')
+
+
+
