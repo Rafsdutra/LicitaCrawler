7016244057109ce460e4f88660be3752d4019f3e
Rafael <rafsdutra32@gmail.com>
2019-04-10 11:29:47 -0300

    Pipeline SendMail agora funcionando

--- NCrawler/pipelines.py
+++ NCrawler/pipelines.py
@@ -8,41 +8,55 @@
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from scrapy.exceptions import DropItem
 import smtplib
-
-
-
 
 
 class FilterDatePipeline(object):
     def process_item(self, item, spider):
+
         now = datetime.now()
         date_licitacao = int(item['numerocp'][0].split('/')[-1])
         if int(date_licitacao) == now.year:
             print(date_licitacao)
-            return item
-        else:
-            raise DropItem("Licitação fora do ano atual")
+            # return item
+        # else:
+        #     raise DropItem("Licitação fora do ano atual")
 
 
-class sendMail(object):
+class SendMail(object):
+
+
+
+    def open_spider(self, spider):
+        print("######### Iniciando o spider... #########")
+
+
+
+    def process_item(self, item, spider):
+
+     print("######## Processando pipelines... ##########")
+     self.modalidade = str(item['modalidade'])
+     self.objetivo = str(item['objetivo'])
+     self.numerocp = str(item['numerocp'])
+     return item
+
+
 
     def close_spider(self, spider):
 
-        print('Enviando Email com as informações de Licitação...')
+
         from_email = "b89b239862ecb5"
-        to_email = "to@smtp.mailtrap.io"
+        to_email = "rafsdutra32@gmail.com"
 
         msg = MIMEMultipart()
         msg['From'] = from_email
         msg['To'] = to_email
         msg['Subject'] = 'Licitacoes '
 
-        intro = "Licitacoes: \n\n"
-
-        body = 'Oi'
-        body = intro + body
+        nomePrefeitura = spider.name
+        intro = "Licitacoes da Prefeitura de " + nomePrefeitura + '\n\n'
+        # body = 'Oi ' + intro
+        body = intro + 'Modalidade: ' + self.modalidade + '\n' + 'Objetivo: ' + " ".join((self.objetivo.split()))  + '\n'+ 'Numero CP: ' + self.numerocp + '\n\n'
         msg.attach(MIMEText(body, 'plain'))
 
         server = smtplib.SMTP("smtp.mailtrap.io", 2525)
@@ -52,7 +66,13 @@ class sendMail(object):
         server.sendmail(from_email, to_email, text)
         server.quit()
 
-        print('Email enviado! ')
+
+        print('Email Enviado!!')
+
+
+        print('######## Fechando spider...#########')
+
+
 
 
 
